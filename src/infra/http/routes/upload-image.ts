1d9bf5ee580353500dfbcb8fa620c8376ce4fe27
Geovane Borba <geovanne@live.com>
2025-01-21 10:06:35 -0300

    feat: implement route for uploading images with validation, error handling and swagger docs

---
+++ src/infra/http/routes/upload-image.ts
@@ -0,0 +1,64 @@
+import { uploadImage } from '@/app/functions/upload-image';
+import { isRight, unwrapEither } from '@/shared/either';
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
+import { z } from 'zod';
+
+export const uploadImageRoute: FastifyPluginAsyncZod = async (server) => {
+  server.post(
+    '/uploads',
+    {
+      schema: {
+        summary: 'Upload an image',
+        consumes: ['multipart/form-data'],
+        tags: ['uploads'],
+
+        response: {
+          201: z.null().describe('File uploaded successfully'),
+          400: z.object({
+            message: z.string().describe('File is required'),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const uploadedFile = await request.file({
+        limits: {
+          fileSize: 1024 * 1024 * 2, // 2MB
+        },
+      });
+
+      if (!uploadedFile) {
+        return reply.status(400).send({
+          message: 'File is required',
+        });
+      }
+
+      const result = await uploadImage({
+        fileName: uploadedFile.filename,
+        contentType: uploadedFile.mimetype,
+        contentStream: uploadedFile.file,
+      });
+
+      if (uploadedFile.file.truncated) {
+        return reply.status(400).send({
+          message: 'File size limits reached',
+        });
+      }
+
+      if (isRight(result)) {
+        console.log(unwrapEither(result));
+
+        return reply.status(201).send();
+      }
+
+      const error = unwrapEither(result);
+
+      switch (error.constructor.name) {
+        case 'InvalidFileFormatError':
+          return reply.status(400).send({
+            message: error.message,
+          });
+      }
+    }
+  );
+};
